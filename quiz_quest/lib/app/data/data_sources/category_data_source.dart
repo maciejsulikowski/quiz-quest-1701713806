427ab117a0a55e535557e2b55c0fb4c8013926fc
macieksulikowski <suleymaniana99@gmail.com>
2023-12-10 13:47:09 +0100

    Add example SportCategory

--- quiz_quest/lib/app/data/data_sources/category_data_source.dart
+++ quiz_quest/lib/app/data/data_sources/category_data_source.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 
 class CategoriesDataSource {
+  
   Future<Map<String, dynamic>?> categoryData() async {
     final response = await Dio().get(
         'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple');
@@ -11,6 +12,18 @@ class CategoriesDataSource {
       return null;
     }
     
+    return responseData;
+  }
+
+  Future<Map<String, dynamic>?> getSportCategory() async {
+    final response = await Dio().get('https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple');
+
+    final responseData = response.data;
+
+    if (responseData == null) {
+      return null;
+    }
+
     return responseData;
   }
 }

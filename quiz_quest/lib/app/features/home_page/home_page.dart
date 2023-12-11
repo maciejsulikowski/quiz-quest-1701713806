4bda273bbb35aac5b2947b7b7ae04e0bf6cf3fbe
macieksulikowski <suleymaniana99@gmail.com>
2023-12-11 23:17:07 +0100

    Add flutter_bloc package

--- quiz_quest/lib/app/features/home_page/home_page.dart
+++ quiz_quest/lib/app/features/home_page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:quiz_quest/app/features/quiz_pages/first_quiz_page.dart';
 import 'package:quiz_quest/app/features/user_page/user_account.dart';
@@ -58,94 +59,97 @@ class QuizzPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 10, 58, 214),
-            Color.fromARGB(255, 22, 20, 129),
-          ],
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
+    return BlocProvider(
+      create: (context) => SubjectBloc(),
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 10, 58, 214),
+              Color.fromARGB(255, 22, 20, 129),
+            ],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
         ),
-      ),
-      child: ListView(
-        children: [
-          const SizedBox(
-            height: 30,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Hi, user 👋',
-                  style: GoogleFonts.aBeeZee(
-                      fontSize: 24,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                ),
-                const CircleAvatar(
-                  child: Icon(Icons.question_mark),
-                )
-              ],
+        child: ListView(
+          children: [
+            const SizedBox(
+              height: 30,
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  '''Let's play''',
-                  style: GoogleFonts.aBeeZee(
-                      fontSize: 46,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 30),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32.0),
-            child: Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                    colors: [
-                      Color.fromARGB(255, 94, 128, 239),
-                      Color.fromARGB(255, 76, 75, 167),
-                    ],
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Hi, user 👋',
+                    style: GoogleFonts.aBeeZee(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
                   ),
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.red),
-              child: Text(
-                'Points: 100💎',
-                style: GoogleFonts.aBeeZee(
-                    fontSize: 28,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
+                  const CircleAvatar(
+                    child: Icon(Icons.question_mark),
+                  )
+                ],
               ),
             ),
-          ),
-          const SizedBox(height: 30),
-          const CategoryQuizz(),
-          const SizedBox(height: 30),
-          const CategoryQuizz(),
-          const SizedBox(height: 30),
-          const CategoryQuizz(),
-          const SizedBox(height: 30),
-          const CategoryQuizz(),
-          const SizedBox(height: 30),
-        ],
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '''Let's play''',
+                    style: GoogleFonts.aBeeZee(
+                        fontSize: 46,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32.0),
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                      colors: [
+                        Color.fromARGB(255, 94, 128, 239),
+                        Color.fromARGB(255, 76, 75, 167),
+                      ],
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.red),
+                child: Text(
+                  'Points: 100💎',
+                  style: GoogleFonts.aBeeZee(
+                      fontSize: 28,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            const CategoryQuizz(),
+            const SizedBox(height: 30),
+            const CategoryQuizz(),
+            const SizedBox(height: 30),
+            const CategoryQuizz(),
+            const SizedBox(height: 30),
+            const CategoryQuizz(),
+            const SizedBox(height: 30),
+          ],
+        ),
       ),
     );
   }

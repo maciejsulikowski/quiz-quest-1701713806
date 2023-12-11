a60e206f7a6d9723bd6a2811d6ff21e063735ca7
macieksulikowski <suleymaniana99@gmail.com>
2023-12-11 23:06:34 +0100

    Change name in home_state

--- quiz_quest/lib/app/features/home_page/cubit/home_state.dart
+++ quiz_quest/lib/app/features/home_page/cubit/home_state.dart
@@ -2,12 +2,12 @@ part of 'home_cubit.dart';
 
 class HomeState {
   HomeState({
-    this.results = const [],
+    this.cateogriesModel = const [],
     this.isLoading = false,
     this.errorMessage,
   });
 
-  final List<CategoriesModel> results;
+  final List<CategoriesModel> cateogriesModel;
   final bool isLoading;
   final String? errorMessage;
 }

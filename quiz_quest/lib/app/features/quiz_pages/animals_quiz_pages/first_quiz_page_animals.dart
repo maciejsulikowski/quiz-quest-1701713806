f18b0ab03a617ddeb01b9818b0d6655a56fed378
macieksulikowski <suleymaniana99@gmail.com>
2024-01-24 00:29:26 +0100

    Modify getAnimalsCategory to better download data from API

--- quiz_quest/lib/app/features/quiz_pages/animals_quiz_pages/first_quiz_page_animals.dart
+++ quiz_quest/lib/app/features/quiz_pages/animals_quiz_pages/first_quiz_page_animals.dart
@@ -41,113 +41,99 @@ class QuizzPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) =>
-          AnimalsCubit(QuizRepository(QuizCategoriesDataSource()))
-            ..getAnimalsCategory(),
-      child: BlocBuilder<AnimalsCubit, AnimalsState>(
-        builder: (context, state) {
-          final animalsModel = state.animalsQuizModel;
-
-          return Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 10, 58, 214),
-                  Color.fromARGB(255, 22, 20, 129),
-                ],
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
-              ),
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 10, 58, 214),
+            Color.fromARGB(255, 22, 20, 129),
+          ],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+        ),
+      ),
+      child: ListView(
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          Center(
+            child: CircleAvatar(
+              radius: 35,
+              backgroundImage: AssetImage(image),
             ),
-            child: ListView(
-              children: [
-                const SizedBox(
-                  height: 30,
-                ),
-                Center(
-                  child: CircleAvatar(
-                    radius: 35,
-                    backgroundImage: AssetImage(image),
-                  ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Center(
-                  child: Text(
-                    'Rules',
-                    style: GoogleFonts.aBeeZee(
-                        fontSize: 46,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const TextWidget(text: 'You have 20 second to answer'),
-                const SizedBox(
-                  height: 30,
-                ),
-                const TextWidget(text: 'You have 3 lives ❤️❤️❤️'),
-                const SizedBox(
-                  height: 30,
-                ),
-                const TextWidget(text: '1 good answer = 10 points 💎'),
-                const SizedBox(
-                  height: 30,
-                ),
-                const TextWidget(
-                    text:
-                        'Go as far as you can and keep moving forward  🔥 🧨'),
-                const SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 20),
-                  decoration: BoxDecoration(
-                      gradient: const LinearGradient(
-                        colors: [
-                          Color.fromRGBO(143, 165, 255, 1),
-                          Color.fromRGBO(10, 53, 132, 1),
-                        ],
-                        begin: Alignment.centerLeft,
-                        end: Alignment.centerRight,
-                      ),
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(25.0),
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          spreadRadius: 4,
-                          blurRadius: 10,
-                          offset: const Offset(0, 3),
-                        )
-                      ]),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => QuestionQuizPage(
-                                animalsQuizModel: animalsModel,
-                              )));
-                    },
-                    style: ElevatedButton.styleFrom(
-                        minimumSize: const Size.fromHeight(50),
-                        backgroundColor: Colors.transparent,
-                        shadowColor: Colors.transparent),
-                    child: Text('''Ready? Let's go!''',
-                        style: GoogleFonts.aBeeZee(
-                          fontSize: 24,
-                          color: Colors.white,
-                        )),
-                  ),
-                ),
-              ],
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Center(
+            child: Text(
+              'Rules',
+              style: GoogleFonts.aBeeZee(
+                  fontSize: 46,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
             ),
-          );
-        },
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const TextWidget(text: 'You have 20 second to answer'),
+          const SizedBox(
+            height: 30,
+          ),
+          const TextWidget(text: 'You have 3 lives ❤️❤️❤️'),
+          const SizedBox(
+            height: 30,
+          ),
+          const TextWidget(text: '1 good answer = 10 points 💎'),
+          const SizedBox(
+            height: 30,
+          ),
+          const TextWidget(
+              text: 'Go as far as you can and keep moving forward  🔥 🧨'),
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  colors: [
+                    Color.fromRGBO(143, 165, 255, 1),
+                    Color.fromRGBO(10, 53, 132, 1),
+                  ],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                ),
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(25.0),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    spreadRadius: 4,
+                    blurRadius: 10,
+                    offset: const Offset(0, 3),
+                  )
+                ]),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const QuestionQuizPage()));
+              },
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                  backgroundColor: Colors.transparent,
+                  shadowColor: Colors.transparent),
+              child: Text('''Ready? Let's go!''',
+                  style: GoogleFonts.aBeeZee(
+                    fontSize: 24,
+                    color: Colors.white,
+                  )),
+            ),
+          ),
+        ],
       ),
     );
   }
--- quiz_quest/lib/app/features/quiz_pages/animals_quiz_pages/question_quiz_page_animals.dart
+++ quiz_quest/lib/app/features/quiz_pages/animals_quiz_pages/question_quiz_page_animals.dart
@@ -2,6 +2,7 @@ import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:quiz_quest/app/core/enums.dart';
 import 'package:quiz_quest/app/data/data_sources/quiz_data_source/quiz_categories_data_source.dart';
 import 'package:quiz_quest/app/domain/models/animals_model/animals_quiz_model.dart';
 import 'package:quiz_quest/app/domain/repositories/quiz_repository/quiz_repository.dart';
@@ -12,11 +13,8 @@ import 'package:quiz_quest/app/features/quiz_pages/quiz_countdown_timer/quiz_cou
 
 class QuestionQuizPage extends StatefulWidget {
   const QuestionQuizPage({
-    required this.animalsQuizModel,
     super.key,
   });
-
-  final AnimalsQuizModel? animalsQuizModel;
 
   @override
   State<QuestionQuizPage> createState() => _QuestionQuizPageState();
@@ -25,154 +23,189 @@ class QuestionQuizPage extends StatefulWidget {
 class _QuestionQuizPageState extends State<QuestionQuizPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: QuizzPage(
-          animalsQuizModel: widget.animalsQuizModel,
-        ),
-      ),
-    );
-  }
-}
-
-class QuizzPage extends StatefulWidget {
-  const QuizzPage({
-    required this.animalsQuizModel,
-    super.key,
-  });
-
-  final AnimalsQuizModel? animalsQuizModel;
-
-  @override
-  State<QuizzPage> createState() => _QuizzPageState();
-}
-
-class _QuizzPageState extends State<QuizzPage> {
-  int currentIndex = 0;
-  late String question;
-  late List<dynamic> list;
-  late String correctAnswer;
-  final controller = CountDownController();
-  bool isButtonBlocked = true;
-
-  void updateAnswers() {
-    question = widget.animalsQuizModel!.results[currentIndex].question;
-    List incorrectAnswers =
-        widget.animalsQuizModel!.results[currentIndex].incorrectAnswers;
-    correctAnswer =
-        widget.animalsQuizModel!.results[currentIndex].correctAnswer;
-    list = incorrectAnswers + [correctAnswer];
-    list.shuffle();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    updateAnswers();
-  }
-
-  @override
-  Widget build(BuildContext context) {
     const int duration = 21;
     Color color = Colors.white;
+    // int currentIndex = 0;
+    // late String question;
+    // late List<dynamic> list;
+    // late String correctAnswer;
+    final controller = CountDownController();
+    bool isButtonBlocked = true;
 
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 10, 58, 214),
-            Color.fromARGB(255, 22, 20, 129),
-          ],
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-        ),
-      ),
-      child: ListView(
-        children: [
-          const SizedBox(
-            height: 15,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const HomePage()));
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.white, width: 2.0),
-                    ),
-                    padding: const EdgeInsets.all(10.0),
-                    child: const Icon(Icons.close, color: Colors.white),
-                  ),
+    return BlocProvider(
+      create: (context) =>
+          AnimalsCubit(QuizRepository(QuizCategoriesDataSource()))
+            ..getAnimalsCategory(),
+      child: BlocBuilder<AnimalsCubit, AnimalsState>(
+        builder: (context, state) {
+          if (state.status == Status.loading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          final animalsModel = state.animalsQuizModel;
+
+          return Scaffold(
+            body: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(255, 10, 58, 214),
+                    Color.fromARGB(255, 22, 20, 129),
+                  ],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
                 ),
               ),
-            ],
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          CountDownTimer(duration: duration, controller: controller),
-          const SizedBox(
-            height: 15,
-          ),
-          if (widget.animalsQuizModel != null)
-            QuestionWidget(
-              question: question,
-            ),
-          const SizedBox(
-            height: 30,
-          ),
-          if (widget.animalsQuizModel != null)
-            for (final answer in list) ...[
-              AnswerWidget(
-                isButtonBlocked: isButtonBlocked,
-                color: color,
-                controller: controller,
-                answer: answer,
-                isCorrectAnswer: answer == correctAnswer ? true : false,
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-            ],
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                    color: Colors.black,
-                    child: ElevatedButton(
-                      onPressed: isButtonBlocked
-                          ? null
-                          : () {
-                              controller.start();
-                              setState(() {
-                                currentIndex = currentIndex + 1;
-                              });
-                            },
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.black,
+              child: ListView(
+                children: [
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const HomePage()));
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border:
+                                  Border.all(color: Colors.white, width: 2.0),
+                            ),
+                            padding: const EdgeInsets.all(10.0),
+                            child: const Icon(Icons.close, color: Colors.white),
+                          ),
+                        ),
                       ),
-                      child: const Text('Next Question ➔'),
-                    )),
-              ],
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  CountDownTimer(duration: duration, controller: controller),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  if (animalsModel != null)
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: ListView.builder(
+                        itemCount: 1,
+                        shrinkWrap: true,
+                        physics: const ClampingScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          final correctAnswer =
+                              animalsModel.results[index].correctAnswer;
+                          final incorrectAnswers =
+                              animalsModel.results[index].incorrectAnswers;
+                          final list = incorrectAnswers + [correctAnswer];
+                          list.shuffle();
+
+                          return Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              QuestionWidget(
+                                question: animalsModel.results[index].question,
+                              ),
+                              const SizedBox(
+                                height: 30,
+                              ),
+                              AnswerWidget(
+                                // isButtonBlocked: isButtonBlocked,
+                                // color: color,
+                                // controller: controller,
+                                answer: list,
+                                // isCorrectAnswer: answer == correctAnswer ? true : false,
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              const SizedBox(
+                                height: 30,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 20.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Container(
+                                        color: Colors.black,
+                                        child: ElevatedButton(
+                                          onPressed: () {},
+                                          //isButtonBlocked
+                                          //     ? null
+                                          //     : () {
+                                          //         controller.start();
+                                          //         setState(() {
+                                          //           currentIndex =
+                                          //               currentIndex + 1;
+                                          //         });
+                                          //       },
+                                          style: ElevatedButton.styleFrom(
+                                            foregroundColor: Colors.white,
+                                            backgroundColor: Colors.black,
+                                          ),
+                                          child: const Text('Next Question ➔'),
+                                        )),
+                                  ],
+                                ),
+                              )
+                            ],
+                          );
+                        },
+                      ),
+                    )
+                ],
+              ),
             ),
-          )
-        ],
+          );
+        },
       ),
     );
   }
 }
+
+// class QuizzPage extends StatefulWidget {
+//   const QuizzPage({
+//     super.key,
+//   });
+
+//   @override
+//   State<QuizzPage> createState() => _QuizzPageState();
+// }
+
+// class _QuizzPageState extends State<QuizzPage> {
+//   int currentIndex = 0;
+//   late String question;
+//   late List<dynamic> list;
+//   late String correctAnswer;
+//   final controller = CountDownController();
+//   bool isButtonBlocked = true;
+
+//   // void updateAnswers() {
+//   //   question = widget.animalsQuizModel!.results[currentIndex].question;
+//   //   List incorrectAnswers =
+//   //       widget.animalsQuizModel!.results[currentIndex].incorrectAnswers;
+//   //   correctAnswer =
+//   //       widget.animalsQuizModel!.results[currentIndex].correctAnswer;
+//   //   list = incorrectAnswers + [correctAnswer];
+//   //   list.shuffle();
+//   // }
+
+//   // @override
+//   // void initState() {
+//   //   super.initState();
+//   //   updateAnswers();
+//   // }
+// }
 
 class QuestionWidget extends StatelessWidget {
   const QuestionWidget({
@@ -198,24 +231,98 @@ class QuestionWidget extends StatelessWidget {
 class AnswerWidget extends StatefulWidget {
   AnswerWidget({
     required this.answer,
-    required this.isCorrectAnswer,
-    required this.controller,
-    required this.color,
-    required this.isButtonBlocked,
+    // required this.isCorrectAnswer,
+    // required this.controller,
+    // required this.color,
+    // required this.isButtonBlocked,
     super.key,
   });
 
-  final String answer;
-  final bool isCorrectAnswer;
-  final CountDownController controller;
-  Color color;
-  final bool isButtonBlocked;
+  final List answer;
+  // final bool isCorrectAnswer;
+  // final CountDownController controller;
+  // Color color;
+  // final bool isButtonBlocked;
 
   @override
   State<AnswerWidget> createState() => _AnswerWidgetState();
 }
 
 class _AnswerWidgetState extends State<AnswerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        AnswerButton(
+          answer: widget.answer[0],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        AnswerButton(
+          answer: widget.answer[1],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        AnswerButton(
+          answer: widget.answer[2],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        AnswerButton(
+          answer: widget.answer[3],
+        ),
+
+        // Container(
+        //   margin: const EdgeInsets.symmetric(horizontal: 20),
+        //   decoration: BoxDecoration(
+        //       gradient: const LinearGradient(
+        //         colors: [
+        //           Color.fromRGBO(143, 165, 255, 1),
+        //           Color.fromRGBO(10, 53, 132, 1),
+        //         ],
+        //         begin: Alignment.centerLeft,
+        //         end: Alignment.centerRight,
+        //       ),
+        //       borderRadius: const BorderRadius.all(
+        //         Radius.circular(6.0),
+        //       ),
+        //       boxShadow: [
+        //         BoxShadow(
+        //           color: Colors.black.withOpacity(0.1),
+        //           spreadRadius: 4,
+        //           blurRadius: 10,
+        //           offset: const Offset(0, 3),
+        //         )
+        //       ]),
+        //   child: ElevatedButton(
+        //     onPressed: () {},
+        //     style: ElevatedButton.styleFrom(
+        //         minimumSize: const Size.fromHeight(50),
+        //         backgroundColor: Colors.transparent,
+        //         shadowColor: Colors.transparent),
+        //     child: Text(widget.firstBadAnswer,
+        //         style: GoogleFonts.aBeeZee(
+        //           fontSize: 24,
+        //           color: Colors.white,
+        //         )),
+        //   ),
+        // ),
+      ],
+    );
+  }
+}
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({
+    super.key,
+    required this.answer,
+  });
+
+  final String answer;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -241,27 +348,15 @@ class _AnswerWidgetState extends State<AnswerWidget> {
             )
           ]),
       child: ElevatedButton(
-        onPressed: () {
-          if (widget.isCorrectAnswer) {
-            setState(() {
-              widget.color = Colors.green;
-              widget.controller.pause();
-            });
-          } else {
-            setState(() {
-              widget.color = Colors.red;
-              widget.controller.pause();
-            });
-          }
-        },
+        onPressed: () {},
         style: ElevatedButton.styleFrom(
             minimumSize: const Size.fromHeight(50),
             backgroundColor: Colors.transparent,
             shadowColor: Colors.transparent),
-        child: Text(widget.answer,
+        child: Text(answer,
             style: GoogleFonts.aBeeZee(
               fontSize: 24,
-              color: widget.color,
+              color: Colors.white,
             )),
       ),
     );

08d2a4cb6ceaea09b04245211eb1e833ef13ca0a
macieksulikowski <suleymaniana99@gmail.com>
2024-02-28 10:57:17 +0100

    Map data for AchievementModel i AchievementRepository

--- quiz_quest/lib/app/domain/repositories/achievement_repository/achievement_repository.dart
+++ quiz_quest/lib/app/domain/repositories/achievement_repository/achievement_repository.dart
@@ -1,5 +1,6 @@
 import 'package:injectable/injectable.dart';
 import 'package:quiz_quest/app/data/data_sources/achievements_data_source/achievements_data_source.dart';
+import 'package:quiz_quest/app/domain/models/achievements_model/achievements_model.dart';
 
 @injectable
 class AchievementRepository {
@@ -7,9 +8,21 @@ class AchievementRepository {
 
   final AchievementDataSource achievementDataSource;
 
+  Stream<AchievementModel> getAchievementsModel() {
+    final data = achievementDataSource.getAchievementsData();
 
-  
-
+    return data.map((doc) {
+      return AchievementModel(
+        userID: doc['user_id'],
+        isFirstAchievementReady: doc['is_first_achievement_ready'],
+        isSecondAchievementReady: doc['is_second_achievement_ready'],
+        isThirdAchievementReady: doc['is_third_achievement_ready'],
+        isFourthAchievementReady: doc['is_fourth_achievement_ready'],
+        isFifthAchievementReady: doc['is_fifth_achievement_ready'],
+        isSixthAchievementReady: doc['is_sixth_achievement_ready'],
+      );
+    });
+  }
 
   Future<void> setFalseAchievements() async {
     return achievementDataSource.setFalseAchievements();

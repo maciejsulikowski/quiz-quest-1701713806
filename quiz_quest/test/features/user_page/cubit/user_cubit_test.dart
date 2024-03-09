92a902117766d7a80ca74e8d3f3f422db22bcb88
macieksulikowski <suleymaniana99@gmail.com>
2024-03-09 11:38:02 +0100

    Create tests for UserCubit for every single method

--- quiz_quest/test/features/user_page/cubit/user_cubit_test.dart
+++ quiz_quest/test/features/user_page/cubit/user_cubit_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -11,15 +13,19 @@ class MockUserRepository extends Mock implements UserRepository {}
 
 class MockRankingRepository extends Mock implements RankingRepository {}
 
+class MockStreamSubscription extends Mock implements StreamSubscription {}
+
 void main() {
   late MockUserRepository userRepository;
   late MockRankingRepository rankingRepository;
   late UserCubit sut;
+  late MockStreamSubscription streamSubscription;
 
   setUp(() {
     userRepository = MockUserRepository();
     rankingRepository = MockRankingRepository();
     sut = UserCubit(userRepository, rankingRepository);
+    streamSubscription = MockStreamSubscription();
   });
 
   final userData = UserModel(
@@ -69,5 +75,288 @@ void main() {
         ],
       );
     });
+  });
+
+  group('updateRankingName', () {
+    group('succes', () {
+      setUp(() {
+        when(() => userRepository.updateRankingName('Maciek')).thenAnswer(
+          (_) async => {},
+        );
+      });
+
+      blocTest(
+        'should call userRepository.updateRankingName() method',
+        build: () => sut,
+        act: (cubit) => cubit.updateRankingName('Maciek'),
+        expect: () => [
+          UserState(status: Status.success, isSaved: true),
+        ],
+      );
+    });
+
+    group('failure', () {
+      setUp(() {
+        when(() => userRepository.updateRankingName('Maciek'))
+            .thenThrow(Exception('test-exception-error'));
+      });
+
+      blocTest(
+        'should call userRepository.updateRankingName() method',
+        build: () => sut,
+        act: (cubit) => cubit.updateRankingName('Maciek'),
+        expect: () => [
+          UserState(
+              status: Status.error,
+              errorMessage: 'Exception: test-exception-error'),
+        ],
+      );
+    });
+  });
+
+  group('updateName', () {
+    group('succes', () {
+      setUp(() {
+        when(() => userRepository.updateName('Maciek')).thenAnswer(
+          (_) async => {},
+        );
+      });
+
+      blocTest(
+        'should call userRepository.updateName() method',
+        build: () => sut,
+        act: (cubit) => cubit.updateName('Maciek'),
+        expect: () => [
+          UserState(status: Status.loading),
+          UserState(status: Status.success, isSaved: true),
+        ],
+      );
+    });
+
+    group('failure', () {
+      setUp(() {
+        when(() => userRepository.updateName('Maciek'))
+            .thenThrow(Exception('test-exception-error'));
+      });
+
+      blocTest(
+        'should call userRepository.updateName() method',
+        build: () => sut,
+        act: (cubit) => cubit.updateName('Maciek'),
+        expect: () => [
+          UserState(status: Status.loading),
+          UserState(
+              status: Status.error,
+              errorMessage: 'Exception: test-exception-error'),
+        ],
+      );
+    });
+  });
+
+  group('updateSurname', () {
+    group('succes', () {
+      setUp(() {
+        when(() => userRepository.updateSurname('Suli')).thenAnswer(
+          (_) async => {},
+        );
+      });
+
+      blocTest(
+        'should call userRepository.updateSurname() method',
+        build: () => sut,
+        act: (cubit) => cubit.updateSurname('Suli'),
+        expect: () => [
+          UserState(status: Status.loading),
+          UserState(status: Status.success, isSaved: true),
+        ],
+      );
+    });
+
+    group('failure', () {
+      setUp(() {
+        when(() => userRepository.updateSurname('Suli'))
+            .thenThrow(Exception('test-exception-error'));
+      });
+
+      blocTest(
+        'should call userRepository.updateSurname() method',
+        build: () => sut,
+        act: (cubit) => cubit.updateSurname('Suli'),
+        expect: () => [
+          UserState(status: Status.loading),
+          UserState(
+              status: Status.error,
+              errorMessage: 'Exception: test-exception-error'),
+        ],
+      );
+    });
+  });
+
+  group('updateGender', () {
+    group('succes', () {
+      setUp(() {
+        when(() => userRepository.updateGender('Male')).thenAnswer(
+          (_) async => {},
+        );
+      });
+
+      blocTest(
+        'should call userRepository.updateGender() method',
+        build: () => sut,
+        act: (cubit) => cubit.updateGender('Male'),
+        expect: () => [
+          UserState(status: Status.loading),
+          UserState(status: Status.success, isSaved: true),
+        ],
+      );
+    });
+
+    group('failure', () {
+      setUp(() {
+        when(() => userRepository.updateGender('Male'))
+            .thenThrow(Exception('test-exception-error'));
+      });
+
+      blocTest(
+        'should call userRepository.updateGender() method',
+        build: () => sut,
+        act: (cubit) => cubit.updateGender('Male'),
+        expect: () => [
+          UserState(status: Status.loading),
+          UserState(
+              status: Status.error,
+              errorMessage: 'Exception: test-exception-error'),
+        ],
+      );
+    });
+  });
+
+  group('updateCategory', () {
+    group('succes', () {
+      setUp(() {
+        when(() => userRepository.updateCategory('Films')).thenAnswer(
+          (_) async => {},
+        );
+      });
+
+      blocTest(
+        'should call userRepository.updateCategory() method',
+        build: () => sut,
+        act: (cubit) => cubit.updateCategory('Films'),
+        expect: () => [
+          UserState(status: Status.loading),
+          UserState(status: Status.success, isSaved: true),
+        ],
+      );
+    });
+
+    group('failure', () {
+      setUp(() {
+        when(() => userRepository.updateCategory('Films'))
+            .thenThrow(Exception('test-exception-error'));
+      });
+
+      blocTest(
+        'should call userRepository.updateCategory() method',
+        build: () => sut,
+        act: (cubit) => cubit.updateCategory('Films'),
+        expect: () => [
+          UserState(status: Status.loading),
+          UserState(
+              status: Status.error,
+              errorMessage: 'Exception: test-exception-error'),
+        ],
+      );
+    });
+  });
+
+  group('updateImage', () {
+    group('succes', () {
+      setUp(() {
+        when(() => userRepository.updateImage('image_url')).thenAnswer(
+          (_) async => {},
+        );
+      });
+
+      blocTest(
+        'should call userRepository.updateImage() method',
+        build: () => sut,
+        act: (cubit) => cubit.updateImage('image_url'),
+        expect: () => [
+          UserState(status: Status.loading),
+          UserState(status: Status.success, isSaved: true),
+        ],
+      );
+    });
+
+    group('failure', () {
+      setUp(() {
+        when(() => userRepository.updateImage('image_url'))
+            .thenThrow(Exception('test-exception-error'));
+      });
+
+      blocTest(
+        'should call userRepository.updateImage() method',
+        build: () => sut,
+        act: (cubit) => cubit.updateImage('image_url'),
+        expect: () => [
+          UserState(status: Status.loading),
+          UserState(
+              status: Status.error,
+              errorMessage: 'Exception: test-exception-error'),
+        ],
+      );
+    });
+  });
+
+  group('changeUserBool', () {
+    group('succes', () {
+      setUp(() {
+        when(() => userRepository.changeUserBool()).thenAnswer(
+          (_) async => {},
+        );
+      });
+
+      blocTest(
+        'should call userRepository.changeUserBool() method',
+        build: () => sut,
+        act: (cubit) => cubit.changeUserBool(),
+        expect: () => [
+          UserState(status: Status.loading),
+          UserState(status: Status.success, isSaved: true),
+        ],
+      );
+    });
+
+    group('failure', () {
+      setUp(() {
+        when(() => userRepository.changeUserBool())
+            .thenThrow(Exception('test-exception-error'));
+      });
+
+      blocTest(
+        'should call userRepository.changeUserBool() method',
+        build: () => sut,
+        act: (cubit) => cubit.changeUserBool(),
+        expect: () => [
+          UserState(status: Status.loading),
+          UserState(
+              status: Status.error,
+              errorMessage: 'Exception: test-exception-error'),
+        ],
+      );
+    });
+  });
+
+  test('close cancels the stream subscription', () {
+    when(() => userRepository.getUserModel()).thenAnswer(
+      (_) => const Stream.empty(),
+    );
+    when(() => streamSubscription.cancel()).thenAnswer((_) async {});
+
+    sut.start();
+    sut.close();
+
+    verifyNever(() => streamSubscription.cancel()).called(0);
   });
 }
